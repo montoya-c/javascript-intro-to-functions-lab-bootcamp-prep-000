125d9856282f2a98a8b5e146bb028d07fe875bf4
Cassandra Montoya <montoya.c@outlook.com>
2018-06-01 02:08:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,13 +16,12 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if (whisper(string)===string)){
-    return ("I can\'t hear you!")
+    return ("I can\'t hear you!");
   }
   else if (shout(string)===string)){
-    return ("YES INDEED!")
+    return ("YES INDEED!");
   }
   else if (string==="I love you, Grandma."){
-    return 
+    return ("I love you, too.");
   }
-  
-  }
+}
